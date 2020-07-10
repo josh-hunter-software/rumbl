eb8e9f8847f4d2460ce7de9bc25faf69fb6f989a
Josh Hunter <thatsmegettinmail@yahoo.com>
2020-07-10 13:06:34 -0500

    Add video watching feature

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,34 @@
+/***
+ * Excerpted from "Programming Phoenix 1.4",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material,
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose.
+ * Visit http://www.pragmaticprogrammer.com/titles/phoenix14 for more book information.
+***/
 // We need to import the CSS so that webpack will load it.
-// The MiniCssExtractPlugin is used to separate it out into
+// The ExtractTextPlugin is used to separate it out into
 // its own CSS file.
-import "../css/app.scss"
+import css from "../css/app.scss"
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
 // in "webpack.config.js".
 //
-// Import deps with the dep name or local files with a relative path, for example:
-//
-//     import {Socket} from "phoenix"
-//     import socket from "./socket"
+// Import dependencies
 //
 import "phoenix_html"
+
+// Import local files
+//
+// Local files can be imported directly using relative paths, for example:
+// import socket from "./socket"
+
+import Player from "./player"
+let video = document.getElementById("video")
+
+if(video) {
+  Player.init(video.id, video.getAttribute("data-player-id"), () => {
+    console.log("player ready!")
+  })
+}
