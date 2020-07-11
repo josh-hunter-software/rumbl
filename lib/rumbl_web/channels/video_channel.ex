2d0c194cba11a39a9f055437969b9a01130062ae
Josh Hunter <thatsmegettinmail@yahoo.com>
2020-07-11 00:05:54 -0500

    Implement annotations via channels

---
+++ lib/rumbl_web/channels/video_channel.ex
@@ -0,0 +1,19 @@
+defmodule RumblWeb.VideoChannel do
+  use RumblWeb, :channel
+
+  def join("videos:" <> video_id, _params, socket) do
+    {:ok, socket}
+  end
+
+  def handle_in("new_annotation", params, socket) do
+    # important to build broadcast payload,
+    # not just forward params along i.e. strong params
+    broadcast!(socket, "new_annotation", %{
+      user: %{username: "anon"},
+      body: params["body"],
+      at: params["at"]
+    })
+
+    {:reply, :ok, socket}
+  end
+end
