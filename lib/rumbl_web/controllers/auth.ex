639269357b457d0259b322ba23683f362065f838
Josh Hunter <thatsmegettinmail@yahoo.com>
2020-07-11 07:53:20 -0500

    Use token for channels

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -8,18 +8,18 @@ defmodule RumblWeb.Auth do
 
   def call(conn, _opts) do
     user_id = get_session(conn, :user_id)
+
     cond do
-      conn.assigns[:current_user] ->
-        conn
+      user = conn.assigns[:current_user] ->
+        put_current_user(conn, user)
       user = user_id && Rumbl.Accounts.get_user(user_id) ->
-        assign(conn, :current_user, user)
+        put_current_user(conn, user)
       true ->
         assign(conn, :current_user, nil)
     end
   end
-
   def login(conn, user) do
-    assign(conn, :current_user, user)
+    put_current_user(conn, user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
   end
@@ -36,5 +36,12 @@ defmodule RumblWeb.Auth do
       |> redirect(to: Routes.page_path(conn, :index))
       |> halt()
     end
+  end
+
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    assign(conn, :current_user, user)
+    |> assign(:user_token, token)
   end
 end
